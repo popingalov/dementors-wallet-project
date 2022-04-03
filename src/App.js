477b6810c2cfe2baa60d518d79b574a47a19f2f9
Pavlo Hrendzha <pashahrendzha99@gmail.com>
2022-04-03 19:37:48 +0300

    added nav

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ import authOperations from './redux/auth/auth-operations';
 import authSelectors from 'redux/auth/auth-selectors';
 import PrivateRoute from './helpers/PrivateRoute';
 import PublicRoute from './helpers/PublicRoute';
-//
-//
+
 const HomeView = lazy(() => import('./pages/HomeView'));
 const RegisterView = lazy(() => import('./pages/RegisterView'));
 const LoginView = lazy(() => import('./pages/LoginView'));
@@ -26,47 +25,39 @@ export default function App() {
       {isFetchingCurrentUser ? (
         <h1>Hi world</h1>
       ) : (
-        <>
-          <Suspense fallback={<h1>Крутим спинер</h1>}>
-            <Routes>
-              <Route
-                path="/"
-                element={
-                  <PublicRoute>
-                    <h2>Старт?</h2>
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/register"
-                element={
-                  <PublicRoute restricted>
-                    <h2>Registr</h2>
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/login"
-                element={
-                  <PublicRoute redirectTo="/contacts" restricted>
-                    <h2>login</h2>
-                  </PublicRoute>
-                }
-              />
-              <Route
-                path="/wallet"
-                element={
-                  <PrivateRoute redirectTo="/login">
-                    <h2>wallet?</h2>
-                  </PrivateRoute>
-                }
-              />
-            </Routes>
-          </Suspense>
-        </>
+        <Suspense fallback={<h1>Крутим спинер</h1>}>
+          <Routes>
+            <Route
+              path="/login"
+              element={
+                <PublicRoute redirectTo="/contacts" restricted>
+                  <h2>login</h2>
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="/register"
+              element={
+                <PublicRoute restricted>
+                  <h2>Registr</h2>
+                </PublicRoute>
+              }
+            />
+
+            <Route
+              path="/"
+              element={
+                <PrivateRoute redirectTo="/login">
+                  <HomeView />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Suspense>
       )}
 
-      <ToastContainer autoClose={3000} />
+      {/* <ToastContainer autoClose={3000} /> */}
     </>
   );
 }
