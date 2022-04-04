75cb31ff3d836b9be59b7f1dddc2404521174cfa
Polina Dudchenko <pv.dudchenko@gmail.com>
2022-04-04 23:11:02 +0200

    transaction compnent done

---
+++ src/components/Transaction/Transaction.jsx
@@ -0,0 +1,22 @@
+import { Table, Thead, Tbody, Tr, Th, Td } from 'react-super-responsive-table'
+import PropTypes from 'prop-types'
+
+export default function Transaction({date, type,category, comment, amount, balance}) {
+    return <>
+        <Td>{date}</Td>
+      <Td>{type}</Td>
+      <Td>{category}</Td>
+      <Td>{comment}</Td>
+      <Td>{amount}</Td>
+        <Td>{balance}</Td>
+    </>
+}
+
+Transaction.propTypes = {
+    date: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  comment: PropTypes.string,
+  amount: PropTypes.number.isRequired,
+  balance: PropTypes.number.isRequired
+}
