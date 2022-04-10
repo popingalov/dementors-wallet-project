be89d42466c2492d69a42fdff622c6ada42bbccc
DenisK0s <kostenkoden89@gmail.com>
2022-04-10 22:58:54 +0300

    made unnecessary changes

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,33 +1,20 @@
-<<<<<<< HEAD
+
 import React from "react";
 import s from "./RegistrationPage.module.css";
 // import RegistrationForm from "../../components/RegistrationForm";
 import BackgroundContainer from "components/BackgroundContainer";
-=======
-import React from 'react';
-import s from './RegistrationPage.module.css';
-<<<<<<< HEAD:src/pages/RegistrationPage/RegistrationPage.js
-// import RegistrationForm from '../../components/registrationForm';
-=======
-// import RegistrationForm from "../../components/RegistrationForm";
-import BackgroundContainer from 'components/backgroundContainer/BackgroundContainer';
->>>>>>> 9eba9226b2bfc3137b52c1001d2d3d5d2345c321:src/pages/RegistrationPage/RegistrationPage.jsx
->>>>>>> eb69f7e75ed22d5286118ac4cb4d824eeb8192dd
 
 const RegistrationPage = () => {
   return (
     <BackgroundContainer>
       <div className={s.wrapper}>
-<<<<<<< HEAD:src/pages/RegistrationPage/RegistrationPage.js
         <p className={s.title}>Finance App</p>
         {/* <RegistrationForm /> */}
-=======
         <div className={s.box}>
           <p className={s.title}>Finance App</p>
         </div>
         {/* <RegistrationForm/> */}
         <div className={s.ellipseBlur} />
->>>>>>> 9eba9226b2bfc3137b52c1001d2d3d5d2345c321:src/pages/RegistrationPage/RegistrationPage.jsx
       </div>
     </BackgroundContainer>
   );
